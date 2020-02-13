f38e33d4684e196e0a2e086799cc67e4bfd5370d
NagaPrakash Danda <ndanda@driveshack.com>
2020-02-13 17:27:38 -0500

    Removed unnecessary creation of local variables in viewModel. Using LiveData coroutine to set data on ViewModel

--- app/src/main/java/com/learn/dictionary/synonyms/SynonymsViewModel.kt
+++ app/src/main/java/com/learn/dictionary/synonyms/SynonymsViewModel.kt
@@ -1,26 +1,17 @@
 package com.learn.dictionary.synonyms
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import com.learn.dictionary.model.ApiResult
 import com.learn.dictionary.repository.Repository
-import kotlinx.coroutines.*
 import javax.inject.Inject
 
-class SynonymsViewModel @Inject constructor(val repository: Repository): ViewModel() {
+class SynonymsViewModel @Inject constructor(val repository: Repository) : ViewModel() {
 
     val search: MutableLiveData<String> = MutableLiveData()
     val synonymsList: LiveData<ApiResult> = Transformations.switchMap(search) {
-        val tempList: MutableLiveData<ApiResult> = MutableLiveData()
-        CoroutineScope(Dispatchers.IO).launch {
-            val temp = repository.getSynonym(it)
-            withContext(Dispatchers.Main){
-                tempList.value = temp
-            }
+        liveData {
+            emit(repository.getSynonym(it))
         }
-        tempList
     }
 
     fun setSearchString(key: String) {
